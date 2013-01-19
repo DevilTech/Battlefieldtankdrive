fd7d769494149c48729bd3b0754e15e8b18a4753
Jeremy <1559deviltech@gmail.com>
2013-01-19 10:48:42 -0500

    now drives with the wheel/pedals

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -2,10 +2,7 @@
 package edu.wpi.first.wpilibj.templates;
 
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SimpleRobot;
-import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.*;
 
 
 public class RobotTemplate extends SimpleRobot {
@@ -13,12 +10,20 @@ public class RobotTemplate extends SimpleRobot {
     Victor vlad;
     RobotDrive diego;
     Joystick john;
+    boolean button1;
+    boolean button2;
+    JoyButton butt;
+    double time;
     
     public void robotInit(){
         vicky = new Victor(2);
         vlad = new Victor(3);
         john = new Joystick(1);
         diego = new RobotDrive(vlad, vicky);
+        button1 = false;
+        button2 = true;
+        butt = new JoyButton(john);
+        time = 1.0/16;
     }
     public void autonomous() {
         
@@ -27,8 +32,21 @@ public class RobotTemplate extends SimpleRobot {
    
     public void operatorControl() {
         while(isEnabled()){
-            diego.arcadeDrive(john.getZ(), john.getX(), true);
-            if
+            diego.arcadeDrive(john.getY(), john.getX());
+             if(butt.isNewPress(butt.lback)){
+                vlad.set(.5);
+                vicky.set(.5);
+                Timer.delay(time);
+                vlad.set(0);
+                vicky.set(0);
+            }
+            else if(butt.isNewPress(butt.rback)){
+                vlad.set(-.5);
+                vicky.set(-.5);
+                Timer.delay(time);
+                vicky.set(0);
+                vlad.set(0);
+        }
         }
     }
     
@@ -37,3 +55,4 @@ public class RobotTemplate extends SimpleRobot {
     
     }
 }
+    
